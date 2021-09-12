a185c98eb6053f086d68ba487a223f81c23b47ef
Adrian Bernardi <a-bernardi@hotmail.com>
2021-09-12 11:48:01 -0300

    Adding log to IP location service

--- src/main/java/ar/com/yosuelto/services/LocationService.java
+++ src/main/java/ar/com/yosuelto/services/LocationService.java
@@ -3,6 +3,8 @@ package ar.com.yosuelto.services;
 import ar.com.yosuelto.clients.GeoIPClient;
 import ar.com.yosuelto.model.Location;
 import ar.com.yosuelto.repositories.LocationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +17,12 @@ public class LocationService {
     @Autowired
     private GeoIPClient geoIPClient;
 
+    private Logger logger = LoggerFactory.getLogger(LocationService.class);
+
     public Location saveLocation(String ip) {
         Location location = geoIPClient.getLocation(ip);
+        logger.info("IP: " + ip);
+        logger.info("Location: " + location);
         return locationRepository.save(location);
     }
 
